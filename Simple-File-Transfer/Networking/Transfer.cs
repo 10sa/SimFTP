3b41efd4b8f17aacb5697aee6ab5c62021a4cfaf
Tensa <zenkins26@gmail.com>
2016-12-20 22:08:33 +0900

    Update.

--- Simple-File-Transfer/Networking/Transfer.cs
+++ Simple-File-Transfer/Networking/Transfer.cs
@@ -14,8 +14,9 @@ using System.Net.Sockets;
 namespace Simple_File_Transfer.Networking
 {
 	public delegate void ServerTransferCallback(string address, string statusMessage);
+	public delegate void ClientTransferCallback();
 
-	public class ServerTransfer
+	public class ServerTransfer : IDisposable
 	{
 		#region Private Const field
 		private const int ServerPort = 44332;
@@ -23,7 +24,8 @@ namespace Simple_File_Transfer.Networking
 		#endregion
 
 		#region Private field
-		private Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		private Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		private List<Thread> threadList = new List<Thread>();
 		#endregion
 
 		#region Public Callback field
@@ -34,18 +36,21 @@ namespace Simple_File_Transfer.Networking
 
 		public ServerTransfer()
 		{
-			ServerSocket.Bind(GetBindingData());
-			ServerSocket.Listen(MaximunBacklog);
-			
-			Thread serverThread = new Thread(new ThreadStart(ConnectionHandleRoutine));
+			serverSocket.Bind(GetBindingData());
+			serverSocket.Listen(MaximunBacklog);
+			serverSocket.NoDelay = false;
+
+			threadList.Add(new Thread(new ThreadStart(ConnectionHandleRoutine)));
+			threadList[0].Start();
 		}
 
 		private void ConnectionHandleRoutine()
 		{
-			Socket clientSocket = ServerSocket.Accept();
+			Socket clientSocket = serverSocket.Accept();
 			ConnectingCallback(clientSocket.RemoteEndPoint.ToString(), "Client Connected.");
 
-			Thread clinetHandleThread = new Thread(new ParameterizedThreadStart(ClientHandleRoutine));
+			threadList.Add(new Thread(new ParameterizedThreadStart(ClientHandleRoutine)));
+			threadList.Last().Start();
 		}
 
 		private void ClientHandleRoutine(object threadArgs)
@@ -56,6 +61,59 @@ namespace Simple_File_Transfer.Networking
 		private IPEndPoint GetBindingData()
 		{
 			return new IPEndPoint(IPAddress.Any, ServerPort);
+		}
+		
+		public void Dispose()
+		{
+			serverSocket.Dispose();
+
+			foreach(var thread in threadList)
+			{
+				// blocking?
+				thread.Interrupt();
+				threadList.Remove(thread);
+			}
+
+			return;
+		}
+	}
+
+
+
+	public class ClientTransfer : IDisposable
+	{
+		private const int ServerPort = 44332;
+		private Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		public event ClientTransferCallback ConnectingCallback;
+		public event ClientTransferCallback ConnectedCallback;
+
+		public ClientTransfer(string address)
+		{
+			try
+			{
+				ConnectingCallback();
+				clientSocket.Connect(address, ServerPort);
+				ConnectedCallback();
+
+				clientSocket.NoDelay = false;
+			}
+			catch(SocketException) { throw; }
+		}
+
+		public void SendingFile(byte[] file)
+		{
+			clientSocket.SendBufferSize = (int)file.LongLength / 4;
+			clientSocket.send
+		}
+
+		public void Dispose()
+		{
+			if(clientSocket.Connected)
+				clientSocket.Disconnect(false);
+
+			clientSocket.Dispose();
+			return;
 		}
 	}
 }
