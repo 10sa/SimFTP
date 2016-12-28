46ff7517ecc7d422fe35b8c17a70bfb28e498e16
10sa <zenkins26@gmail.com>
2016-12-28 22:13:25 +0900

    Update.

---
+++ Simple-File-Transfer/Net/DataFrame.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Simple_File_Transfer.Net
+{
+	public class DataFrame
+	{
+		public short FileNameLenght { get; private set; }
+		public long FileSize { get; private set; }
+
+		public string FileName { get; private set; }
+		public byte[] FileData { get; private set; }
+
+		private DataFrame() { }
+
+		public DataFrame(short nameLenght, long fileSize, string fileName, byte[] fileData)
+		{
+			this.FileNameLenght = nameLenght;
+			this.FileSize = fileSize;
+			this.FileName = fileName;
+			this.FileData = fileData;
+
+			return;
+		}
+
+		public byte[] GetBinaryData()
+		{
+			byte[][] argArray = { BitConverter.GetBytes(FileNameLenght), BitConverter.GetBytes(FileSize), Encoding.UTF8.GetBytes(FileName), FileData };
+			byte[] buffer = new byte[sizeof(short) + sizeof(long) + GetByteCount(FileName) + FileData.Length];
+
+			long dataLength = 0;
+			foreach (var arg in argArray)
+				dataLength = BulidArray(arg, buffer, dataLength);
+
+			return buffer;
+		}
+
+		protected long BulidArray(byte[] soruceBuffer, byte[] orignalBuffer, long length)
+		{
+			Array.Copy(soruceBuffer, 0, orignalBuffer, length, soruceBuffer.LongLength);
+			return length + soruceBuffer.LongLength;
+		}
+
+		protected int GetByteCount(string s)
+		{
+			return Encoding.UTF8.GetByteCount(s);
+		}
+	}
+
+	public class BasicSecurityDataFrame : DataFrame
+	{
+		public byte[] Checksum { get; private set; }
+
+		public BasicSecurityDataFrame(string fileName, short nameLenght, byte[] fileData, long fileSize) : base(nameLenght, fileSize, fileName, fileData)
+		{
+			SetChecksum(fileData);
+			return;
+		}
+
+		private void SetChecksum(byte[] fileData)
+		{
+			using (SHA256CryptoServiceProvider hash = new SHA256CryptoServiceProvider())
+			{
+				Checksum = hash.ComputeHash(fileData);
+			}
+		}
+
+		public new byte[] GetBinaryData()
+		{
+			byte[][] argsArray = { base.GetBinaryData(), Checksum };
+			byte[] buffer = new byte[argsArray[0].LongLength + Checksum.LongLength];
+
+			long dataLenght = 0;
+			foreach(var arg in argsArray)
+				dataLenght = BulidArray(arg, buffer, dataLenght);
+
+			return buffer;
+		}
+	}
+}
--- Simple-File-Transfer/Net/PacketFrame.cs
+++ Simple-File-Transfer/Net/PacketFrame.cs
@@ -13,46 +13,6 @@ namespace Simple_File_Transfer.Net
 		ExpertsSecurity
 	}
 
-	public class DataFrame
-	{
-		public short FileNameLenght { get; private set; }
-		public long FileSize { get; private set; }
-
-		public char[] FileName { get; private set; }
-		public byte[] FileData { get; private set; }
-
-		private DataFrame() { }
-
-		public DataFrame(short nameLenght, long fileSize, char[] fileName, byte[] fileData)
-		{
-			this.FileNameLenght = nameLenght;
-			this.FileSize = fileSize;
-			this.FileName = fileName;
-			this.FileData = fileData;
-
-			return;
-		}
-
-		public byte[] GetBinaryData()
-		{
-			byte[][] argArray = { BitConverter.GetBytes(FileNameLenght), BitConverter.GetBytes(FileSize), Encoding.UTF8.GetBytes(FileName, 0, FileName.Length), FileData };
-			byte[] buffer = new byte[sizeof(short) + sizeof(long) + FileName.Length + FileData.Length];
-
-			long dataLength = 0;
-			foreach(var arg in argArray)
-				dataLength = BulidArray(arg, buffer, dataLength);
-
-			return buffer;
-		}
-
-		private long BulidArray(byte[] soruceBuffer, byte[] orignalBuffer, long length)
-		{
-			Array.Copy(soruceBuffer, 0, orignalBuffer, length, soruceBuffer.LongLength);
-			return length + soruceBuffer.LongLength;
-		}
-	}
-
-
 	public class PacketFrame
 	{
 		public PacketType PacketType { get; private set; }
@@ -60,11 +20,14 @@ namespace Simple_File_Transfer.Net
 		public PacketFrame()
 		{
 			this.PacketType = PacketType.BasicFrame;
+
 			return;
 		}
+
 		protected PacketFrame(PacketType packetType)
 		{
 			this.PacketType = packetType;
+
 			return;
 		}
 
@@ -72,24 +35,35 @@ namespace Simple_File_Transfer.Net
 		{
 			return BitConverter.GetBytes((int)this.PacketType);
 		}
+
+		protected int GetByteCount(params string[] strings)
+		{
+			int count = 0;
+			foreach(var str in strings)
+				count += Encoding.UTF8.GetByteCount(str);
+
+			return count;
+		}
 	}
 
 	
 	public class BasicSecurityPacket : PacketFrame
 	{
 		public bool IsAnonynomus { get; private set; }
-		public char[] Username { get; private set; }
-		public char[] Password { get; private set; }
+		public string Username { get; private set; }
+		public string Password { get; private set; }
 		
 		public BasicSecurityPacket() : base(PacketType.BasicSecurity)
 		{
 			SetAnonymous();
+
 			return;
 		}
 
-		public BasicSecurityPacket(char[] username, char[] password) : base(PacketType.BasicSecurity)
+		public BasicSecurityPacket(string username, string password) : base(PacketType.BasicSecurity)
 		{
 			SetAccount(username, password);
+
 			return;
 		}
 
@@ -102,25 +76,34 @@ namespace Simple_File_Transfer.Net
 			return;
 		}
 
-		private void SetAccount(char[] username, char[] password)
+		private void SetAccount(string username, string password)
 		{
 			this.IsAnonynomus = false;
 			this.Username = username;
 			this.Password = password;
+
 			return;
 		}
 
 		public new byte[] GetBinaryData()
 		{
 			byte[] parentData = base.GetBinaryData();
-			byte[] buffer = new byte[GetDataSize() + parentData.Length];
 
-			byte[] binaryUsername = Encoding.UTF8.GetBytes(Username);
+		}
 
-			Array.Copy(parentData, buffer, parentData.Length);
-			Array.Copy(binaryUsername, 0, buffer, buffer.Length, binaryUsername.Length);
+		protected byte[] ByteArrayAttach(byte[] firstArray, byte[] SecondArray)
+		{
+			byte[] buffer = new byte[firstArray.LongLength + SecondArray.LongLength];
+
+			Array.Copy(firstArray, buffer, firstArray.LongLength);
+			Array.Copy(SecondArray, 0, buffer, firstArray.LongLength, SecondArray.LongLength);
 
 			return buffer;
+		}
+
+		private byte[] UTF8GetBytes(string str)
+		{
+			return Encoding.UTF8.GetBytes(str);
 		}
 
 		private int GetDataSize()
@@ -128,37 +111,7 @@ namespace Simple_File_Transfer.Net
 			if(this.IsAnonynomus == true)
 				return sizeof(bool);
 			else
-				return ((sizeof(char) * (this.Username.Length + this.Password.Length)) + sizeof(bool));
-		}
-	}
-
-	public class BasicSecurityDataFrame : DataFrame
-	{
-		public byte[] Checksum { get; private set; }
-
-		public BasicSecurityDataFrame(short nameLenght, long fileSize, char[] fileName, byte[] fileData) : base(nameLenght, fileSize, fileName, fileData)
-		{
-			GetChecksum(fileData);
-			return;
-		}
-
-		private void GetChecksum(byte[] fileData)
-		{
-			using (SHA256CryptoServiceProvider hash = new SHA256CryptoServiceProvider())
-			{
-				Checksum = hash.ComputeHash(fileData);
-			}
-		}
-
-		public new byte[] GetBinaryData()
-		{
-			byte[] parentData = base.GetBinaryData();
-			byte[] buffer = new byte[parentData.LongLength + Checksum.LongLength];
-
-			Array.Copy(parentData, buffer, parentData.LongLength);
-			Array.Copy(Checksum, 0, buffer, parentData.LongLength, Checksum.LongLength);
-
-			return buffer;
+				return (GetByteCount(Username, Password) + sizeof(bool));
 		}
 	}
 
